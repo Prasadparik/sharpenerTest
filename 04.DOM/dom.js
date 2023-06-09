f0cc52f3b6a06d4a4f5f58c094429b0d9ff022a2
Prasad Parik <prasadparik18@gmail.com>
2023-06-09 19:03:33 +0530

    creating and appending in DOM

--- 04.DOM/dom.js
+++ 04.DOM/dom.js
@@ -27,3 +27,29 @@ Items[2].style.backgroundColor = "green";
 Items[2].style.color = "white";
 
 for (let i = 0; i < Items.length; i++) Items[i].style.fontWeight = "bold";
+
+//parentNode=============================
+
+var listItem = document.querySelector("#items");
+
+listItem.parentNode.style.backgroundColor = "#eee";
+
+//parentNode=============================
+
+console.log(listItem.children);
+listItem.children[1].style.backgroundColor = "yellow";
+
+listItem.firstElementChild.innerText = "Hello world";
+
+// Create elements ===================
+
+var newDiv = document.createElement("Div");
+newDiv.className = "hello";
+console.log(newDiv);
+
+var newDivText = document.createTextNode("Hello World");
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
+container.insertBefore(newDiv, h1);
